362c546c8869d785602520439ebee74a39312e20
mdanish101 <34332069+mdanish101@users.noreply.github.com>
2021-04-25 02:41:15 +0530

    ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "vm01"{
+  instance_type = t2.micro
+  ami = data.aws_ami.std_ami.id
+}
+
+data "aws_ami" "std_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0*"]
+  }
+}
